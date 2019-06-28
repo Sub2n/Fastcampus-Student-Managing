1e3210a8ad9b6146d58131a6b7d6d1dbc74f1004
Sub2n <95su1208@gmail.com>
2019-06-28 12:02:39 +0900

    login

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Manager } from '../models/manager';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   isLogin = false;
+  apiUrl = environment.apiUrl;
+  managers: Manager[];
 
-  constructor() {}
+  constructor(public http: HttpClient) {}
+
+  getManagers() {
+    this.http
+      .get<Manager[]>(`${this.apiUrl}/manager`)
+      .subscribe(users => (this.managers = users));
+  }
 
   getLoginStatus(): Observable<boolean> {
     return of(this.isLogin);
